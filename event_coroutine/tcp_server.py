7f24ca86ca495231a28ab6ae1d9f8fc7b5936147
ismewen <ismewen@outlook.com>
2021-09-13 15:52:00 +0800

    tcp server

--- event_coroutine/tcp_server.py
+++ event_coroutine/tcp_server.py
@@ -1,16 +1,29 @@
+from event_coroutine.event_loop import EventLoop
+
+
 class TCPServer(object):
 
     def __init__(self, loop):
         self.loop = loop
         self.listen_sock = self.loop.create_listen_socket()
+        self.loop.add_coroutine(self.server_forever())
 
     async def handle_client(self, sock):
         while True:
-            data = await sock.acc
-
+            data = await sock.recv(1024)
+            if not data:
+                print("client disconnected")
+                break
+            await sock.send(data.upper())
 
     async def server_forever(self):
         while True:
             sock, addr = await self.listen_sock.accept()
             print("client connect addr = {}".format(addr))
             self.loop.add_coroutine(self.handle_client(sock))
+
+
+if __name__ == "__main__":
+    loop = EventLoop.get_instance()
+    TCPServer(loop)
+    loop.run_forever()
